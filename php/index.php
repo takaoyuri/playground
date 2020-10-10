2fa08447ec6aee892d2630aa5138d0ff0930fcf1
Yuri <1169171+takaoyuri@users.noreply.github.com>
2020-10-10 10:51:03 +0900

    up

--- php/index.php
+++ php/index.php
@@ -1,2 +1,8 @@
 <?php
-echo getenv('PG_HOST');
+$host = getenv('PG_HOST');
+$user = getenv('PG_USER');
+
+$PDO = new PDO("pgsql:host={$host}", $user);
+foreach($PDO->query('select * from a') as $row)  {
+  print_r($row);
+}
